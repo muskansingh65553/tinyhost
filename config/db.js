e61e2d85b5d85eaa0081d939218d7210c17e3ca7
muskansingh65553 <71725061+muskansingh65553@users.noreply.github.com>
2025-04-19 15:28:21 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgres://postgres:Kapbytes18@pw4g4cgo40sgs80scw0w0ss4:5432/postgres',
+  connectionString: 'postgres://postgres:postgres@pw4g4cgo40sgs80scw0w0ss4:5432/postgres',
 });
 
 module.exports = {
